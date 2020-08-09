8bbe617c6d7b71f5b608a56bdca01a3954290697
vityajulin <vkdvornokov@gmail.com>
2020-08-09 16:26:54 +0300

    add recycler view

--- app/src/main/java/com/example/firstapplication/Adapter.kt
+++ app/src/main/java/com/example/firstapplication/Adapter.kt
@@ -1,4 +1,166 @@
 package com.example.firstapplication
 
-class Adapter {
+import android.content.Intent
+import android.net.Uri
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.post_card_view.view.*
+import org.joda.time.LocalDate
+import org.joda.time.Period
+
+class Adapter(val list: List<PostCard>) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        val view =
+            LayoutInflater.from(parent.context).inflate(R.layout.post_card_view, parent, false)
+        return PostViewHolder(this, view)
+    }
+
+    override fun getItemCount() = list.size
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        with(holder as PostViewHolder) {
+            bind(list[position])
+        }
+    }
+}
+
+class PostViewHolder(private val adapter: Adapter, view: View) : RecyclerView.ViewHolder(view) {
+    init {
+        with(itemView) {
+            like_btn.setOnClickListener {
+                if (adapterPosition != RecyclerView.NO_POSITION) {
+                    val item = adapter.list[adapterPosition]
+                    item.liked = !item.liked
+
+                    if (item.liked) {
+                        like_btn.setImageResource(R.drawable.ic_baseline_favorite_24_f10f0a)
+                        item.likeCounts += 1
+                        like_counts_txt.text = item.likeCounts.toString()
+                        like_counts_txt.visibility = View.VISIBLE
+                    } else {
+                        like_btn.setImageResource(R.drawable.ic_baseline_favorite_24_808080)
+                        item.likeCounts -= 1
+                        like_counts_txt.text = item.likeCounts.toString()
+                        if (item.likeCounts <= 0) {
+                            like_counts_txt.visibility = View.INVISIBLE
+                        }
+                    }
+                    adapter.notifyItemChanged(adapterPosition)
+                }
+            }
+
+            comment_btn.setOnClickListener {
+                if (adapterPosition != RecyclerView.NO_POSITION) {
+                    val item = adapter.list[adapterPosition]
+                    item.commented = !item.commented
+
+                    if (item.commented) {
+                        item.commentCounts += 1
+                        comments_counts_txt.text = item.commentCounts.toString()
+                        comments_counts_txt.visibility = View.VISIBLE
+                    } else {
+                        item.commentCounts -= 1
+                        comments_counts_txt.text = item.commentCounts.toString()
+                        if (item.commentCounts <= 0) {
+                            comments_counts_txt.visibility = View.INVISIBLE
+                        }
+                    }
+                    adapter.notifyItemChanged(adapterPosition)
+                }
+            }
+
+            share_btn.setOnClickListener {
+                if (adapterPosition != RecyclerView.NO_POSITION) {
+                    val item = adapter.list[adapterPosition]
+                    item.shared = !item.shared
+
+                    if (item.shared) {
+                        item.shareCounts += 1
+                        share_counts_txt.text = item.shareCounts.toString()
+                        share_counts_txt.visibility = View.VISIBLE
+                    } else {
+                        item.shareCounts -= 1
+                        share_counts_txt.text = item.shareCounts.toString()
+                        if (item.shareCounts <= 0) {
+                            share_counts_txt.visibility = View.INVISIBLE
+                        }
+                    }
+                    adapter.notifyItemChanged(adapterPosition)
+                }
+            }
+
+            location_btn.setOnClickListener {
+                if (adapterPosition != RecyclerView.NO_POSITION) {
+                    val item = adapter.list[adapterPosition]
+
+                    if (item.postType == PostType.EVENT) {
+                        val geoLocation = GeoLocation()
+                        val intentUri = Uri.parse(
+                            if (geoLocation.coordinate != "") {
+                                "geo:${geoLocation.coordinate}"
+                            } else {
+                                "geo:0,0?q=${geoLocation.address}"
+                            }
+                        )
+                        val mapIntent = Intent(Intent.ACTION_VIEW, intentUri).apply {
+                            setPackage("com.google.android.apps.maps")
+                            itemView.context.startActivity(this)
+                        }
+                    } else {
+                        Toast.makeText(itemView.context, "Post is not event", Toast.LENGTH_SHORT)
+                            .show()
+                    }
+                }
+            }
+
+            youtube_btn.setOnClickListener {
+                if (adapterPosition != RecyclerView.NO_POSITION) {
+                    val item = adapter.list[adapterPosition]
+
+                    if (item.postType == PostType.YOUTUBE_VIDEO) {
+                        val intentUri = Uri.parse("https://www.youtube.com/watch?v=WhWc3b3KhnY")
+                        val youtubeIntent = Intent(Intent.ACTION_VIEW, intentUri).apply {
+                            setPackage("com.google.android.youtube")
+                            itemView.context.startActivity(this)
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    fun bind(post: PostCard) {
+        with(itemView) {
+            username_txt.text = post.username
+            post_txt.text = post.post
+            date_txt.text = getTimePeriod(post)
+
+            if (post.liked) {
+                like_btn.setImageResource(R.drawable.ic_baseline_favorite_24_f10f0a)
+                like_counts_txt.text = post.likeCounts.toString()
+                like_counts_txt.visibility = View.VISIBLE
+            }
+
+            if (post.commented) {
+                comments_counts_txt.text = post.commentCounts.toString()
+                comments_counts_txt.visibility = View.VISIBLE
+            }
+
+            if (post.shared) {
+                share_counts_txt.text = post.shareCounts.toString()
+                share_counts_txt.visibility = View.VISIBLE
+            }
+        }
+    }
+
+    private fun getTimePeriod(post: PostCard): String {
+        val currentDate = LocalDate()
+        val postDate = post.date
+        val period = Period(postDate, currentDate)
+
+        return "hour " + period.hours + " minutes " + period.minutes + " ago"
+    }
 }
--- app/src/main/java/com/example/firstapplication/MainActivity.kt
+++ app/src/main/java/com/example/firstapplication/MainActivity.kt
@@ -1,137 +1,43 @@
 package com.example.firstapplication
 
-import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
-import android.view.View
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.post_card_view.*
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 import net.danlew.android.joda.JodaTimeAndroid
 import org.joda.time.LocalDate
-import org.joda.time.Period
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         JodaTimeAndroid.init(this)
-        initViews()
 
-        like_btn.setOnClickListener {
-            post.liked = !post.liked
-
-            if (post.liked) {
-                like_btn.setImageResource(R.drawable.ic_baseline_favorite_24_f10f0a)
-                post.likeCounts += 1
-                like_counts_txt.text = post.likeCounts.toString()
-                like_counts_txt.visibility = View.VISIBLE
-            } else {
-                like_btn.setImageResource(R.drawable.ic_baseline_favorite_24_808080)
-                post.likeCounts -= 1
-                like_counts_txt.text = post.likeCounts.toString()
-                if (post.likeCounts <= 0) {
-                    like_counts_txt.visibility = View.INVISIBLE
-                }
-            }
-        }
-
-        comment_btn.setOnClickListener {
-            post.commented = !post.commented
-
-            if (post.commented) {
-                post.commentCounts += 1
-                comments_counts_txt.text = post.commentCounts.toString()
-                comments_counts_txt.visibility = View.VISIBLE
-            } else {
-                post.commentCounts -= 1
-                comments_counts_txt.text = post.commentCounts.toString()
-                if (post.commentCounts <= 0) {
-                    comments_counts_txt.visibility = View.INVISIBLE
-                }
-            }
-        }
-
-        share_btn.setOnClickListener {
-            post.shared = !post.shared
-
-            if (post.shared) {
-                post.shareCounts += 1
-                share_counts_txt.text = post.shareCounts.toString()
-                share_counts_txt.visibility = View.VISIBLE
-            } else {
-                post.shareCounts -= 1
-                share_counts_txt.text = post.shareCounts.toString()
-                if (post.shareCounts <= 0) {
-                    share_counts_txt.visibility = View.INVISIBLE
-                }
-            }
-        }
-
-        location_btn.setOnClickListener {
-            if (post.postType == PostType.EVENT) {
-                val geoLocation = GeoLocation()
-                val intentUri = Uri.parse(
-                    if (geoLocation.coordinate != "") {
-                        "geo:${geoLocation.coordinate}"
-                    } else {
-                        "geo:0,0?q=${geoLocation.address}"
-                    }
-                )
-                val mapIntent = Intent(Intent.ACTION_VIEW, intentUri).apply {
-                    setPackage("com.google.android.apps.maps")
-                    startActivity(this)
-                }
-            } else {
-                Toast.makeText(this, "Post is not event", Toast.LENGTH_SHORT).show()
-            }
-        }
-
-        youtube_btn.setOnClickListener {
-            if (post.postType == PostType.YOUTUBE_VIDEO) {
-                val intentUri = Uri.parse("https://www.youtube.com/watch?v=WhWc3b3KhnY")
-                val youtubeIntent = Intent(Intent.ACTION_VIEW, intentUri).apply {
-                    setPackage("com.google.android.youtube")
-                    startActivity(this)
-                }
-            }
+        with(recycle_main) {
+            layoutManager = LinearLayoutManager(this@MainActivity)
+            adapter = Adapter(list)
         }
     }
 
-    private val post = PostCard(
-        "Username",
-        LocalDate(),
-        "first post in our network",
-        postType = PostType.EVENT
+    private val list = listOf(
+        PostCard(
+            "Username1",
+            LocalDate(),
+            "first post in our network",
+            postType = PostType.EVENT
+        ),
+        PostCard(
+            "Username2",
+            LocalDate(),
+            "second post in our network",
+            postType = PostType.YOUTUBE_VIDEO
+        ),
+        PostCard(
+            "Username3",
+            LocalDate(),
+            "third post in our network",
+            postType = PostType.REPOST
+        )
+
     )
-
-    private fun initViews() {
-        username_txt.text = post.username
-        post_txt.text = post.post
-        date_txt.text = getTimePeriod(post)
-
-        if (post.liked) {
-            like_btn.setImageResource(R.drawable.ic_baseline_favorite_24_f10f0a)
-            like_counts_txt.text = post.likeCounts.toString()
-            like_counts_txt.visibility = View.VISIBLE
-        }
-
-        if (post.commented) {
-            comments_counts_txt.text = post.commentCounts.toString()
-            comments_counts_txt.visibility = View.VISIBLE
-        }
-
-        if (post.shared) {
-            share_counts_txt.text = post.shareCounts.toString()
-            share_counts_txt.visibility = View.VISIBLE
-        }
-    }
-
-    private fun getTimePeriod(post: PostCard): String {
-        val currentDate = LocalDate()
-        val postDate = post.date
-        val period = Period(postDate, currentDate)
-
-        return "hour " + period.hours + " minutes " + period.minutes + " ago"
-    }
 }
